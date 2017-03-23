7039e04a147c5d8ba1299dd9fc67709f5e5d306b
ChatLurking <anneglines@gmail.com>
2017-03-23 11:13:05 -0700

    Added images for projects

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -7,12 +7,24 @@ const Text = styled.h3`
   padding: 20px;
 `;
 
+const Image = styled.img`
+  position: absolute;
+  max-width: 33.3%;
+  max-height: 33.3%;
+  z-index: -1;
+`;
+
 const Wrapper = styled.div`
   background: ${(props) => props.background };
   width: 33.3%;
   padding-bottom: 50vh;
   float: left;
   opacity: .8;
+
+  background-image: ${(props) => `url(${props.image})`};
+  background-repeat: no-repeat;
+  background-position: center;
+  overflow: hidden;
 
   &:hover {
     opacity: 1;
@@ -31,7 +43,7 @@ const Wrapper = styled.div`
 
 const ProjectImage = (props) => {
   return (
-    <Wrapper background={props.background}>
+    <Wrapper background={props.background} image={props.image}>
       <Text>
         {props.name}
       </Text>
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -9,6 +9,11 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import Avalonstar from '../assets/Avalonstar.png';
+import Geoff from '../assets/Geoff.png';
+import Slack from '../assets/Slack.png';
+import Website from '../assets/Website.png';
+import Code from '../assets/Code.png';
 
 const Wrapper = styled.div`
   background-color: #301436;
@@ -19,7 +24,7 @@ const ModalWrapper = styled.div`
   position: absolute;
   margin: 20px;
   background: #FFF;
-  width: 50vw;
+  width: 75vw;
   padding: 30px;
   border-radius: 10px;
   box-shadow: 5px 5px 5px black;
@@ -37,36 +42,42 @@ const defaultProps = [
     id: 0,
     name: 'Avalonstar',
     background: '#123456',
+    image: Avalonstar,
     desc: "For Christmas Avalonstar left his stream to some trusted community members for 24/7 9 days. Our start up was tasked with finding the best solution for people to stream to Avalonstar's Twitch channel without comprimising his stream key. The final solution was to set up ingest servers for people to stream to. These ingest servers used their twitch name as a stream key, this made it easy for us to switch streamers. From the ingest the feed was streamed to a server that then streamed to Twitch. During a lot of the planning I was busy helping admin a 24/7 10 day charity event (iKasperr's Quest For the Cause) so my main role was researching solutions, testing once the solutions were implemented, and transistioning streamers during the event. This event taught me that staying up for almost 20 days is not the best of ideas.",
   },
   {
     id: 1,
     name: 'Daktronics Scoreboard',
-    background: 'rgb(0, 90, 45)',
+    background: '#000',
+    image: Code,
     desc: "I was tasked with making a program that would intercept Daktronic scoreboard packets so that a custom scoreboard could be overlayed onto YouTube streams. The challenging part of this project was little information on how the packets were encrypted and how to tell what information was sent. I did enjoy finding out that my office was still in range of the wireless football scoreboard and being able to scare passing students with the airhorn.",
   },
   {
     id: 2,
     name: 'Geoff',
     background: '#CCCCCC',
+    image: Geoff,
     desc: "My start up was asked to provide a solution that would add bits, subs, resubs, and tips and allocate them to predefined hashtags for voting. This application is limited to only being able to allow 2 or 4 items to be voted on. My role in this project was taking the React/Redux that I learned in the 2 weeks prior and seeing if I could implement it. Though not much of my code made it into the final project, being able to face a real-world problem using my new skills was good experience.",
   },
   {
     id: 3,
     name: 'Mobile Streaming App',
-    background: 'blue',
+    background: '#000',
+    image: Code,
     desc: "After Twitch's announcement of the IRL catagory and seeing not many good mobile streaming options, my start up decided to make our own. In 3 weeks we made an iOS and Android app using react-native. The feature that I worked on the most was chat integration and efficiency. I currently am in charge of developing the Android app and adding new chat features."
   },
   {
     id: 4,
     name: 'Personal Website',
     background: '#6666FF',
+    image: Website,
     desc: "This website is made in React using create-react-app. This is design number 37 (or so), and though I'm not completely happy with the design I decided that this was better than nothing. The repo for this will remain private until I'm done removing all the remnants of its predecessors.",
   },
   {
     id: 5,
     name: 'Slackaholicus',
     background: 'grey',
+    image: Slack,
     desc: "(In progress) Similar to Geoff's in that it is an application for voting. This application adds a moveable Subscriber/Tip box. I used the base of Geoff's system and added the nessasary features.",
   },
 ];
@@ -86,6 +97,7 @@ const ProjectList = () => {
               id={key}
               name={item.name}
               background={item.background}
+              image={item.image}
             />
         </Link>
       )
@@ -105,19 +117,19 @@ const Modal = ({ match, history }) => {
   }
   return (
     <ModalWrapper onClick={back}>
-  <div className='modal'>
-    <h1>{project.name}</h1>
-    <div color={project.background} />
-    <div>{project.desc}</div>
-  </div>
-  <Button onClick={back}>
-    <FontAwesome
-      name='close'
-      size='3x'
-    />
-  </Button>
-</ModalWrapper>
-
+      <div className='modal'>
+        <h1>{project.name}</h1>
+        <img src={project.image} style={{'max-width': '300px', 'max-height': '400px'}}/>
+        <div color={project.background} />
+        <div>{project.desc}</div>
+      </div>
+      <Button onClick={back}>
+        <FontAwesome
+          name='close'
+          size='3x'
+          />
+      </Button>
+    </ModalWrapper>
   );
 }
 
@@ -132,6 +144,7 @@ const ProjectMatch = ({match}) => {
       background={project.background}
       name={project.name}
       key={project.id}
+      image={project.image}
       />
   );
 }
