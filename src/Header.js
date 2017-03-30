d6fe6788c28813e7bc0cddc4107b47ddaa212152
ChatLurking <anneglines@gmail.com>
2017-03-30 03:12:53 -0700

    Removes console warnings

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Brand from './assets/brand.png';
-import FontAwesome from 'react-fontawesome';
 
 const Wrapper = styled.div`
   display: flex;
