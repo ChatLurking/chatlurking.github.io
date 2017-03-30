2110afe54164d477fbf879cedd6b7c8441e44421
ChatLurking <anneglines@gmail.com>
2017-03-30 16:58:01 -0700

    Adds Projects to Appjs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Header from './Header';
 import Separator from './Separator';
-import Cards from './Cards';
+import Projects from './Projects';
 
-import Card from './assets/brand.png';
 
 const Wrapper = styled.div`
 `;
@@ -16,11 +15,7 @@ class App extends Component {
       <Wrapper>
         <Header />
         <Separator />
-        <Cards
-          src={Card}
-          title='Slackaholicus Overlay'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        />
+        <Projects />
       </Wrapper>
     );
   }
