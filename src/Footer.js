7032060015fd5d4bf5ee77368358ae9e4d2fcbd4
ChatLurking <anneglines@gmail.com>
2017-03-31 02:24:47 -0700

    Adds Footer

---
+++ src/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import FontAwesome from 'react-fontawesome';
+import 'font-awesome/css/font-awesome.min.css';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  background: #222;
+  width: 100%;
+`;
+
+const Button = styled.a`
+  background: transparent;
+  border: none;
+  color: #FFF;
+  padding: 20px 0;
+  cursor: pointer;
+`;
+
+const ButtonHM = styled.button`
+  font-size: 1.25rem;
+  width: 250px;
+  height: 40px;
+  border: solid;
+  border-radius: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+
+  background: #D9AA63;
+  border-color: #D9AA63;
+  color: #000;
+`;
+
+const Footer = () => {
+  return (
+      <Wrapper>
+        <Button href="https://github.com/chatlurking">
+          <FontAwesome
+            name='github'
+            size='4x'
+            />
+        </Button>
+        <ButtonHM>Hire Me</ButtonHM>
+      </Wrapper>
+  );
+}
+
+export default Footer;
